b06157c07e8034bc59a083e2def0e757674ecb5b
김종하 <81893082+gbr7780@users.noreply.github.com>
2022-09-14 22:16:41 +0900

    메인 폼 수정

--- src/main/java/com/kiwi/config/SecurityConfig.java
+++ src/main/java/com/kiwi/config/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig {
                 .logoutSuccessUrl("/");
 
         http.authorizeRequests()
-                .mvcMatchers("/css/**","/js/**","/img/**").permitAll()
+                .mvcMatchers("/css/**","/js/**","/img/**","/video/**").permitAll()
                 .mvcMatchers("/","/members/**","/item/**","/images/**").permitAll()
                 .mvcMatchers("/admin/**").hasRole("ADMIN")
                 .anyRequest().authenticated();
