9be4955c1c6658def2075d5643d0de207f42576a
minsol <minsol@DESKTOP-UJM3099>
2022-10-28 22:55:45 +0900

    민솔 매치리스트 프론트 진행중(날짜 캐러셀 해야함)

---
+++ src/main/java/com/kiwi/match/controller/MatchController.java
@@ -0,0 +1,19 @@
+package com.kiwi.match.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
+
+@RequestMapping("/match")
+@Controller
+public class MatchController {
+
+	// 매치 메인 - 매치 리스트
+	@GetMapping("/matchList")
+	public String matchList() {
+		return "match/matchList";
+	}
+
+}
